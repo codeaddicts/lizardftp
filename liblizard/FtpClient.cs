a83c2a728481cfb5c8c0cfb255a446a6d5c8eefd
Splitty <splittydev@gmail.com>
2016-01-05 03:06:24 +0100

    Added all reply codes specified in rfc 959

--- liblizard/FtpClient.cs
+++ liblizard/FtpClient.cs
@@ -92,7 +92,7 @@ namespace Codeaddicts.Lizard
             ns.Dispose ();
         }
 
-        void AcceptLine (StreamReader reader)
+        void AcceptLine (TextReader reader)
         {
             var line = reader.ReadLine ();
             int code = 0;
@@ -104,8 +104,8 @@ namespace Codeaddicts.Lizard
             }
             dash = line.Length > 3 && line [3] == '-';
             string message = line
-				.Substring (3 + (dash ? 1 : 0))
-				.TrimStart (' ');
+                .Substring (3 + (dash ? 1 : 0))
+                .TrimStart (' ');
             ProcessMessage (code, dash, message);
         }
 
@@ -113,35 +113,123 @@ namespace Codeaddicts.Lizard
         {
             switch (code) {
             case 110:
-				// Restart marker reply
+                // Restart marker reply
                 break;
-            case 211:
-				// System status, or system help reply
+            case 120:
+                // Service ready in nnn minutes
+                break;
+            case 125:
+                // Data connection already open; transfer starting
+                break;
+            case 150:
+                // File status okay; about to open data connection
                 break;
             case 200:
-				// Command okay
+                // Command okay
                 break;
             case 202:
-				// Command not implemented, superfluous at this site
+                // Command not implemented, superfluous at this site
+                break;
+            case 211:
+                // System status, or system help reply
+                break;
+            case 212:
+                // Directory status
+                break;
+            case 213:
+                // File status
+                break;
+            case 214:
+                // Help message
+                break;
+            case 215:
+                // NAME system type
                 break;
             case 220:
-				// Service ready for new user
+                // Service ready for new user
+                break;
+            case 221:
+                // Service closing control connection
+                break;
+            case 225:
+                // Data connection open; no transfer in progress
+                break;
+            case 226:
+                // Closing data connection
+                break;
+            case 227:
+                // Entering passive mode (h1,h2,h3,h4,p1,p2)
+                break;
+            case 230:
+                // User logged in, proceed
+                break;
+            case 250:
+                // Requested file action okay, completed
+                break;
+            case 257:
+                // "PATHNAME" created
+            case 331:
+                // User name okay, need password
+                break;
+            case 332:
+                // Need account for login
+                break;
+            case 350:
+                // Requested file action pending further information
+            case 421:
+                // Service not available, closing control connection
+                break;
+            case 425:
+                // Can't open data connection
+                break;
+            case 426:
+                // Connection closed; transfer aborted
+                break;
+            case 450:
+                // Requested file action not taken
+                break;
+            case 451:
+                // Requested action aborted. Local error in processing
+                break;
+            case 452:
+                // Requested action not taken
+                // Insufficient storage space in system
                 break;
             case 500:
-				// Syntax error or
-				// Command unrecognized
+                // Syntax error or
+                // Command unrecognized
                 break;
             case 501:
-				// Syntax error in parameters or arguments
+                // Syntax error in parameters or arguments
                 break;
             case 502:
-				// Command not implemented
+                // Command not implemented
                 break;
             case 503:
-				// Bad sequence of commands
+                // Bad sequence of commands
                 break;
             case 504:
-				// Command not implemented for that parameter
+                // Command not implemented for that parameter
+                break;
+            case 530:
+                // Not logged in
+                break;
+            case 532:
+                // Need account for storing files
+                break;
+            case 550:
+                // Requested action not taken
+                break;
+            case 551:
+                // Requested action aborted. Page type unknown
+                break;
+            case 552:
+                // Requested file action aborted
+                // Exceeded storage allocation
+                break;
+            case 553:
+                // Requested action not taken
+                // File name not allowed
                 break;
             }
             Console.WriteLine ("[{0:000}] {1}", code, message);
