63742f18b2627e0e31ad6cd6f412046242ca601e
Splitty <splittydev@gmail.com>
2016-01-05 02:22:43 +0100

    Initial commit
    What has been done?
    - Set up project structure
    - Added library
    - Added cli application
    - Added URI parser for parsing the connection string
    - Added FtpClient class and started working on it
    
    What works?
    - Connecting
    - Sending messages to the server
    - Receiving and parsing messages from the server
    
    FtpClient can be used like TcpClient.
    The connection paramaters can either be supplied when
    instantiating the class, or when calling the Connect function.

---
+++ lizardcli/Options.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Codeaddicts.libArgument.Attributes;
+
+namespace Codeaddicts.Lizard.Cli
+{
+	public class Options
+	{
+		[Argument ("-u")]
+		public string Format;
+	}
+}
+
---
+++ lizardcli/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Codeaddicts.libArgument;
+using Codeaddicts.Lizard;
+
+namespace Codeaddicts.Lizard.Cli
+{
+	class MainClass
+	{
+		readonly Options Options;
+		readonly FtpClient Client;
+
+		public static void Main (string[] args) {
+			new MainClass (args).Main ();
+		}
+
+		public MainClass (string[] args) {
+			Options = ArgumentParser.Parse<Options> (args);
+			Client = new FtpClient (Options.Format);
+		}
+
+		public void Main () {
+			Client.Connect ();
+			Client.Wait ();
+		}
+	}
+}
