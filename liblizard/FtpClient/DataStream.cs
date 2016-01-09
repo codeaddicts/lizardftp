7bbddc294671758fcf05d804aafe65ee6b9b668c
Splitty <splittydev@gmail.com>
2016-01-09 01:08:51 +0100

    Moved DataStream to FtpClient folder

---
+++ liblizard/FtpClient/DataStream.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Codeaddicts.Lizard
+{
+	public class DataStream : IDisposable
+	{
+		public TcpClient Client;
+
+		NetworkStream stream;
+		public NetworkStream Stream {
+			get {
+				if (endpoint == null)
+					throw new Exception ("Datastream not yet open");
+				return stream;
+			}
+		}
+
+		IPEndPoint endpoint;
+		public IPEndPoint EndPoint {
+			get {
+				return endpoint;
+			}
+			set {
+				if (value == null)
+					return;
+				endpoint = value;
+				Connect ();
+			}
+		}
+
+		public bool Connected { get { return stream != null; } }
+
+		void Connect () {
+			Client = new TcpClient ();
+			try {
+				Client.Connect (EndPoint);
+				stream = Client.GetStream ();
+			} catch (Exception e) {
+				Console.WriteLine (e.Message);
+				Client = null;
+				stream = null;
+				endpoint = null;
+			}
+		}
+
+		public DataStream () {
+			Client = null;
+			stream = null;
+			endpoint = null;
+		}
+
+		public void Close () {
+			stream.Dispose ();
+			Client = null;
+			endpoint = null;
+			stream = null;
+		}
+
+		public void Send (byte[] data) {
+			stream.Write (data, 0, data.Length);
+		}
+
+		#region IDisposable implementation
+
+		public void Dispose () {
+			Close ();
+		}
+
+		#endregion
+	}
+}
+
